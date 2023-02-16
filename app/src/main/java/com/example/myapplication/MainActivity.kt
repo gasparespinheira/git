5b1472965ea8ef0a59ac7eb35455ed1d4ba55eb9
Gaspar Vilas Espinheira <gasparvilasespinheira@gmail.com>
2023-02-16 16:10:05 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,8 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // testou
-        // teste 2
+        //Apaguei os testes.
 
     }
 }
